152fba40a4dab9a947fadf469c1a24e8bb7480a0
JC <jhegs.knightdev@gmail.com>
2018-10-24 01:00:44 +0800

    Added on create function

--- app/src/main/java/testapp/ayannah/jccolumbres/internalstorage/MainActivity.java
+++ app/src/main/java/testapp/ayannah/jccolumbres/internalstorage/MainActivity.java
@@ -2,12 +2,56 @@ package testapp.ayannah.jccolumbres.internalstorage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView output;
+    public static final String FILE_NAME = "lorem_ipsum.csv";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = findViewById(R.id.outputText);
+        output.setText(R.string.ready_to_code);
+    }
+
+    public void onCreateButtonClick(View view) {
+        String content = getString(R.string.lorem_ipsum);
+        FileOutputStream fileOutputStream = null;
+        File file = new File(FILE_NAME);
+
+        try {
+            fileOutputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fileOutputStream.write(content.getBytes());
+            Toast.makeText(this, "File created: " + FILE_NAME, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fileOutputStream == null){
+                    fileOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void onReadButtonClick(View view) {
+    }
+
+    public void onDeleteButtonClick(View view) {
     }
 }
