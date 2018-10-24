7f02260a5300ecd9cc54be98d1b4812cfc932508
Jigs <jhegs.knightdev@gmail.com>
2018-10-24 15:20:36 +0800

    Added permission checks during runtime

--- app/src/main/java/testapp/ayannah/jccolumbres/internalstorage/MainActivity.java
+++ app/src/main/java/testapp/ayannah/jccolumbres/internalstorage/MainActivity.java
@@ -1,5 +1,11 @@
 package testapp.ayannah.jccolumbres.internalstorage;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +19,11 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSION_WRITE = 1001;
     TextView output;
     public static final String FILE_NAME = "lorem_ipsum.csv";
+    private boolean permissionGranted;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCreateButtonClick(View view) {
+        getPermissions();
+
         String content = getString(R.string.lorem_ipsum);
         FileOutputStream fileOutputStream = null;
         File file = new File(FILE_NAME);
@@ -50,15 +61,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onReadButtonClick(View view) {
+        getPermissions();
+
     }
 
     public void onDeleteButtonClick(View view) {
+        getPermissions();
+
         File file = new File(getFilesDir(), FILE_NAME);
         if (file.exists()){
             deleteFile(FILE_NAME);
             Toast.makeText(this, "File deleted", Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(this, "File not existing", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
+    }
+
+    // Initiate request for permissions.
+    private boolean checkPermissions() {
+
+        if (!isExternalStorageReadable() || !isExternalStorageWritable()) {
+            Toast.makeText(this, "This app only works on devices with usable external storage",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_WRITE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Handle permissions result
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_WRITE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                    Toast.makeText(this, "External storage permission granted",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "You must grant permission!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    public void getPermissions(){
+        if (!permissionGranted){
+            checkPermissions();
+            return;
         }
     }
 }
